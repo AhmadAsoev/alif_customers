5e0c973a7a96082ecd8ea8064a47b0a22726d2a0
AhmadAsoev <asoev.ahmad@mail.ru>
2024-07-06 18:17:55 +0500

    add adder component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from "./page/home/Home";
+import NotFound from './page/notFound/NotFound';
+
 function App() {
   return (
     <div className="App">
+      <RouterProvider
+        router={createBrowserRouter([
+          {
+            path: '/',
+            element: (
+                <Home />
+            ),
+          },
+          {
+            path: '*',
+            element: <NotFound />,
+          },
+        ])}
+      />
     </div>
   );
 }
